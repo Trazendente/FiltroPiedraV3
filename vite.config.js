4855aed71ac9285e5409978097639b20cf6336eb
Trazendente <trazendente.studio@gmail.com>
2025-01-08 00:36:35 -0500

    AJ

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/FiltroPiedraV3/', // Nombre de tu repositorio
+  build: {
+    outDir: 'docs', // Mantén la carpeta de salida como "docs"
+  },
+});
